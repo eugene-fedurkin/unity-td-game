32d9100223eee75f3cbf6f8b1cc5c843a4918c01
Eugene Fedurkin <eugene.fedurkin@gmail.com>
2023-01-16 17:52:18 +0300

    feat: add rotation for unit

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -23,11 +23,28 @@ public class UnitMovement : MonoBehaviour {
             positionTo = pathManager.getNextCoordinate(transform.position);
             if (positionTo == null) {
                 Destroy(gameObject);
+            } else {
+                rotateUnit();
             }
         }
 
         if (positionTo != null) {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(positionTo.x, transform.position.y, positionTo.z), speed / 10 * Time.deltaTime);
         }
+    }
+
+    void rotateUnit() {
+        transform.rotation = Quaternion.LookRotation(new Vector3(positionTo.x, transform.position.y, positionTo.z) - new Vector3(transform.position.x, transform.position.y, transform.position.z));
+
+        /*
+        if (positionTo.x < transform.position.x) {
+            transform.eulerAngles = new Vector3(0, -90, 0);
+        } else if (positionTo.x > transform.position.x) { 
+            transform.eulerAngles = new Vector3(0, 90, 0);
+        } else if (positionTo.z > transform.position.z) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (positionTo.z < transform.position.z) {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }*/
     }
 }
